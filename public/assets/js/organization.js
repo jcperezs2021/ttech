49e34362f97086abc4c0fba7b389495e2373c497
jcperezs2021 <jcperezs2021@gmail.com>
2025-03-18 22:47:00 -0600

    Feat: Org Ext-Cont

--- public/assets/js/organization.js
+++ public/assets/js/organization.js
@@ -1,20 +1,52 @@
 $(document).ready(function() {
-    $('#chart-container').append(`<i class="oci oci-spinner spinner"></i>`);
-    $.ajax({
-        'url': base_url + 'organization/data',
-        'dataType': 'json'
-    })
-    .done(function(data) {
-        datascource = data;
-        $('#chart-container').orgchart({
-            'visibleLevel': 2,
-            'data': data,
-            'nodeContent': 'title',
-            'createNode': function($node, data) {
-                if (data.img) {
-                    $node.prepend('<img class="node-img" src="' + data.img + '" alt="' + data.name + '">');
+
+    function showTwoLevels() {
+        $.ajax({
+            'url': base_url + 'organization/data',
+            'dataType': 'json'
+        })
+        .done(function(data) {
+            datascource = data;
+            $('#chart-container').empty(); // Limpia el contenedor del organigrama
+            $('#chart-container').orgchart({
+                'visibleLevel': 2,
+                'data': data,
+                'nodeContent': 'title',
+                'createNode': function($node, data) {
+                    if (data.img) {
+                        $node.prepend('<img class="node-img" src="' + data.img + '" alt="' + data.name + '">');
+                    }
                 }
-            }
+            });
         });
+    }
+
+    // Despliegue inicial
+    showTwoLevels();
+
+    // Reconstruir el organigrama y mostrar todos los niveles
+    $('#showAll').click(function() {
+        $.ajax({
+            'url': base_url + 'organization/data',
+            'dataType': 'json'
+        })
+        .done(function(data) {
+            $('#chart-container').empty(); // Limpia el contenedor del organigrama
+            $('#chart-container').orgchart({
+                'visibleLevel': 999, // Muestra todos los niveles
+                'data': data,
+                'nodeContent': 'title',
+                'createNode': function($node, data) {
+                    if (data.img) {
+                        $node.prepend('<img class="node-img" src="' + data.img + '" alt="' + data.name + '">');
+                    }
+                }
+            });
+        });
+    });
+
+    // showTwo 
+    $('#showTwo').click(function() {
+        showTwoLevels();
     });
 });
