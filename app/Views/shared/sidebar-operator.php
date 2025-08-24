9680134a4ab10b523c74e16709fc6168a3b26ffe
jcperezs2021 <jcperezs2021@gmail.com>
2025-08-24 02:08:10 -0600

    Fix: Sidebar roles

--- app/Views/shared/sidebar-operator.php
+++ app/Views/shared/sidebar-operator.php
@@ -1,51 +1,92 @@
 <!-- Sidebar Start -->
-<aside class="left-sidebar">
+<aside class="left-sidebar left-sidebar-collapse" id="left__sidebar">
   <div>
     <div class="brand-logo d-flex align-items-center justify-content-between">
-      <a href="<?= base_url('organization') ?>" class="text-nowrap logo-img">
-        <img src="<?= base_url('assets/images/logos/logo-1.png') ?>" height="40" alt="" />
+      <a href="<?= base_url('organization') ?>" class="text-nowrap logo-img py-4">
+        <img src="<?= base_url('assets/images/logos/logo-1.png') ?>" height="45" alt="" />
       </a>
       <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
         <i class="ti ti-x fs-8"></i>
       </div>
     </div>
-    <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
-      <ul id="sidebarnav">
-        <li class="nav-small-cap">
-          <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-          <span class="hide-menu">Intranet</span>
-        </li>
-        <li class="sidebar-item">
-          <a class="sidebar-link" href="<?= base_url('organization') ?>" aria-expanded="false">
-            <span>
-                <i class="ti ti-directions"></i>
-            </span>
-            <span class="hide-menu">Organigramas</span>
-          </a>
-        </li>
-        <li class="sidebar-item">
-          <a class="sidebar-link" href="<?=base_url('/auth/logout')?>" aria-expanded="false">
-            <span>
-                <i class="ti ti-login"></i>
-            </span>
-            <span class="hide-menu">Salir</span>
-          </a>
-        </li>
-        <li class="nav-small-cap">
-          <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-          <span class="hide-menu">ADMINISTRADOR</span>
-        </li>
-        <li class="sidebar-item">
-          <a class="sidebar-link" href="<?= base_url('user') ?>" aria-expanded="false">
-            <span>
-              <i class="ti ti-users"></i>
-            </span>
-            <span class="hide-menu">Empleados</span>
-          </a>
-        </li>
-      </ul>
+    <nav class="sidebar-nav scroll-sidebar mt-2" data-simplebar="">
+      <div class="accordion" id="accordionSidebar">
+        <!-- Intranet -->
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingIntranet">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseIntranet" aria-expanded="true" aria-controls="collapseIntranet">
+              Intranet
+            </button>
+          </h2>
+          <div id="collapseIntranet" class="accordion-collapse collapse show" aria-labelledby="headingIntranet" data-bs-parent="#accordionSidebar">
+            <div class="accordion-body px-0 py-1">
+              <ul class="list-unstyled mb-0">
+                <li class="sidebar-item">
+                  <a class="sidebar-link" href="<?= base_url('organization') ?>">
+                    <i class="ti ti-directions"></i> Organigramas
+                  </a>
+                </li>
+                <li class="sidebar-item">
+                  <a class="sidebar-link" href="<?=base_url('/auth/logout')?>" aria-expanded="false">
+                    <span>
+                        <i class="ti ti-login"></i>
+                    </span>
+                    <span class="hide-menu">Salir</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <!-- ADMINISTRADOR -->
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingAdmin">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseAdmin" aria-expanded="false" aria-controls="collapseAdmin">
+              Administrador
+            </button>
+          </h2>
+          <div id="collapseAdmin" class="accordion-collapse collapse" aria-labelledby="headingAdmin" data-bs-parent="#accordionSidebar">
+            <div class="accordion-body px-0 py-1">
+              <ul class="list-unstyled mb-0">
+                <li class="sidebar-item">
+                  <a class="sidebar-link" href="<?= base_url('user') ?>">
+                    <i class="ti ti-users"></i> Empleados
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Collapse Sidebar -->
+      <div class="sidebar__collapse" id="sidebar__collapse__container">
+        <div id="" class="accordion-collapse">
+          <div class="accordion-body py-1">
+            <ul class="list-unstyled mb-0">
+              <li class="sidebar-item">
+                <a class="sidebar-link" href="#" id="sidebar__collapse__btn">
+                  <i class="ti ti-layout-sidebar-left-collapse"></i> Ocultar
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <!-- Expand Sidebar -->
+      <div class="sidebar__expand" id="sidebar__expand__container">
+        <div id="" class="accordion-collapse">
+          <div class="accordion-body py-1">
+            <ul class="list-unstyled mb-0">
+              <li class="sidebar-item">
+                <a class="sidebar-link sidebar-link-expand" href="#" id="sidebar__expand__btn">
+                  <i class="ti ti-layout-sidebar-left-expand"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </nav>
   </div>
 </aside>
-
-<div class="body-wrapper">
+<div class="body-wrapper body-wrapper__min body-wrapper-collapse" id="body__wrapper">
